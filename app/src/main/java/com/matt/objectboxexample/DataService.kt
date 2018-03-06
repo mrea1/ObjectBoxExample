9946e3958c4307c157475d91df69f36eaa48cc0e
jmieczkowskiWeedMaps <35239884+jmieczkowskiWeedMaps@users.noreply.github.com>
2018-03-06 12:56:55 -0700

    Feature/updating how to init (#1)
    * updated how to init the DataService
    
    * added type
    
    * added fancy method
    
    * convertted to inline for performance
    
    * added missing data, clean up

--- app/src/main/java/com/matt/objectboxexample/DataService.kt
+++ app/src/main/java/com/matt/objectboxexample/DataService.kt
@@ -1,41 +1,48 @@
 package com.matt.objectboxexample
 
+import android.content.Context
 import io.objectbox.Box
+import io.objectbox.BoxStore
 import io.objectbox.kotlin.boxFor
+import io.objectbox.query.Query
 import io.objectbox.query.QueryBuilder
 
 /**
  * Created by matt on 3/1/2018.
  */
 
-open class DataService<T : Any>(app: App) {
+open class DataService<T>(clazz: Class<T>) {
 
-    val boxStore = app.boxStore
-    var box: Box<T>? = null
+    companion object {
+        private lateinit var boxStore: BoxStore
 
-    private fun <V> checkStarted(func: (box: Box<T>) -> V) : V {
-        if (box == null) {
-            throw IllegalStateException("You must call start() before using the service")
-        } else {
-           return func(box!!)
+        fun init(context: Context) {
+            boxStore = MyObjectBox.builder().androidContext(context).build()
         }
     }
 
-    fun getAll(): List<T> = checkStarted { it.all as List<T> }
+    private val box: Box<T> = boxStore.boxFor(clazz)
 
-    fun add(vararg data: T) = checkStarted { it.put(data.asList()) }
+    fun getAll(): List<T> = box.all
 
-    fun delete(vararg data: T) = checkStarted { it.remove(data.toList()) }
+    fun getFilteredData(config: (QueryBuilder<T>) -> QueryBuilder<T>): List<T>{
+        val query = getQuery(config)
 
-    fun queryBuilder() = checkStarted { it.query() }
+        val data = query.find()
+        query.close()
 
-    fun executeQuery(builder: QueryBuilder<T>) = checkStarted { builder.build().find() }
+        return data
+    }
 
-    fun deleteAll() = checkStarted { it.removeAll() }
+    fun getQuery(config: (QueryBuilder<T>) -> QueryBuilder<T>): Query<T> {
+        val builder = box.query()
+        return config(builder).build()
+    }
 
+    fun add(vararg data: T) = box.put(data.asList())
+
+    fun delete(vararg data: T) = box.remove(data.toList())
+
+    fun deleteAll() = box.removeAll()
 }
 
-inline fun <reified T : Any> DataService<T>.start() : DataService<T> {
-    box = boxStore.boxFor()
-    return this
-}
--- app/src/main/java/com/matt/objectboxexample/MainActivity.kt
+++ app/src/main/java/com/matt/objectboxexample/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dataService = DataService<Person>(application as App).start()
+        val dataService = DataService(Person::class.java)
 
         dataService.deleteAll()
 
@@ -20,16 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG,"Full list: " + dataService.getAll())
 
-        Log.d(TAG,"All names starting with with A: " + dataService.queryBuilder()?.startsWith(Person_.name, "A")?.build()?.find())
+        Log.d(TAG,"All names starting with with A: " +
+                dataService.getFilteredData({
+                    it.startsWith(Person_.name, "A")
+                })
+        )
 
         Log.d(TAG,"All names starting with with A or S: " +
-                dataService.queryBuilder()
-                        ?.startsWith(Person_.name, "S")
-                        ?.or()
-                        ?.startsWith(Person_.name, "A")
-                        ?.build()
-                        ?.find())
+                dataService.getFilteredData({
+                    it.startsWith(Person_.name, "S")
+                            .or()
+                            .startsWith(Person_.name, "A")
+                })
+        )
 
-        Log.d(TAG,"Find Andrew: " + dataService.queryBuilder()?.equal(Person_.name, "Andrew")?.build()?.find())
+        Log.d(TAG,"Find Andrew: " +
+                dataService.getFilteredData({
+                    it.equal(Person_.name, "Andrew")
+                })
+        )
     }
 }
